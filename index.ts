2b1642250b5dac38542acf3509f97e32ba95ec0e
SmilinJasper <d.sjd.sjj@gmail.com>
2026-05-22 03:00:33 +0530

    fix: convert _id to string in note schema transformation

--- index.ts
+++ index.ts
@@ -29,7 +29,7 @@ const noteSchema = new mongoose.Schema<INote>({
 
 noteSchema.set('toJSON', {
   transform(document, returnedObject: any) {
-    returnedObject.id = returnedObject._id
+    returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
   },
