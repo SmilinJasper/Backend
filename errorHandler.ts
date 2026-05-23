29596eab1bd0158226259440f8794c18583c569f
SmilinJasper <d.sjd.sjj@gmail.com>
2026-05-24 01:13:09 +0530

    fix: improve error messages in error handling middleware

--- errorHandler.ts
+++ errorHandler.ts
@@ -1,17 +1,16 @@
-import type { Request, Response, NextFunction} from 'express'
+import type { Request, Response, NextFunction } from "express"
 
 export const errorHandler = (error: any, request: Request, response: Response, next: NextFunction) => {
-
+    
     console.error(error)
 
     if(error.name === 'CastError') {
-        response.status(400).json({'Error': 'Malformatted request'})
+        response.status(400).json({'Error': 'Request contains malformatted ID!'})
     }
 
     if(error.name === 'ValidationError') {
-        response.status(400).status(400).json({'Error': 'Missing fields'})
+        response.status(400).json({'Error': error.message})
     }
 
     next(error)
-
 }
