9e1db98ba02832d9eff3cdde8cf8dc741facabab
SmilinJasper <d.sjd.sjj@gmail.com>
2026-05-27 02:06:14 +0530

    fix(middleware): correct typo in error message

--- middlewares/unknownEndpointHandler.ts
+++ middlewares/unknownEndpointHandler.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from 'express'
 
 export const unknownEndpointHandler = (request: Request, response: Response) => {
-    response.status(404).json({'error': 'API Endopoint does not exist!'})
+    response.status(404).json({'error': 'API Endpoint does not exist!'})
 }
